ff0ef4d0f2c9dc129f251ca0fa98cbce3d930aad
Raster728 <rafael.figueiredo2001@gmail.com>
2024-04-17 23:23:29 +0100

    Student Class

--- beginner/src/main/java/com/web/beginner/BeginnerApplication.java
+++ beginner/src/main/java/com/web/beginner/BeginnerApplication.java
@@ -1,13 +1,28 @@
 package com.web.beginner;
 
+import com.web.beginner.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class BeginnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeginnerApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+				new Student(1L, "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21)
+		);
 	}
 
 }
---
+++ beginner/src/main/java/com/web/beginner/student/StudentController.java
@@ -0,0 +1,4 @@
+package com.web.beginner.student;
+
+public class StudentController {
+}
