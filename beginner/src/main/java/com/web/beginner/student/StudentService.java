53ed7a30dc3148cef383530fdaa1a23956d20d44
Raster728 <rafael.figueiredo2001@gmail.com>
2024-04-18 15:10:04 +0100

    Populate student table

--- beginner/src/main/java/com/web/beginner/student/StudentService.java
+++ beginner/src/main/java/com/web/beginner/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.web.beginner.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudent() {
-        return List.of(
-                new Student(1L, "Rita Silva", "anaaritasilva@gmail.com", LocalDate.of(2003, Month.JUNE, 18), 20)
-        );
+        return studentRepository.findAll();
     }
 
 }
